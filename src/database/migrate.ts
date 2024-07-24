555143db0f397b9bfa2ebed04aa3c242cf48226a
izzeddin62 <i.ishimwe@alustudent.com>
2024-07-24 14:08:55 +0200

    debuug

--- src/database/migrate.ts
+++ src/database/migrate.ts
@@ -2,8 +2,11 @@ import { drizzle } from 'drizzle-orm/postgres-js';
 import { migrate } from 'drizzle-orm/postgres-js/migrator';
 import { config } from 'dotenv';
 import postgres = require('postgres');
+console.log(process.env, '++++++++++++');
 config();
 
+
+console.log(process.env, '=========');
 const { DB_HOST, DB_PORT, DB_USER, DB_PASSWORD, DB_NAME } = process.env;
 
 const dbUrl = `postgresql://${DB_USER}${DB_PASSWORD ? `:${DB_PASSWORD}` : ''}@${DB_HOST}:${DB_PORT}/${DB_NAME}`;
